6042d0824a5101b10919ba39288f602f97b366f9
likaiweb <869546886@qq.com>
2017-03-17 01:24:01 +0800

    apple

---
+++ apple/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/3/16.
+ */
+$(function () {
+    $(".fu>.icon").click(function () {
+        $(".son").css("height","650px");
+    })
+    $(".close").click(function () {
+        $(".son").css("height","0");
+    })
+})
